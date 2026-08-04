f542d7f5c0a7f4f4d5838361c09bcc4b33dbaaaa
shrushtitambe048-design <shrushtitambe048@gmail.com>
2026-08-04 18:34:41 +0530

    DAY1 Task

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,8 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
         System.out.println("Welcome to the Java Track!");
+        System.out.println("I ready know how to build programming logic");
+        System.out.println("Now I am learning how to express that logic using java.");
+        System.out.println("Now I know how to usegit and github");
     }
 }
