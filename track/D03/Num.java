dd7e1e4607ec2408407cffb3e0d443e6961d9abd
shrushtitambe048-design <shrushtitambe048@gmail.com>
2026-08-04 19:37:48 +0530

    DAY3 Task

---
+++ track/D03/Num.java
@@ -0,0 +1,17 @@
+package track.D03;
+
+public class Num {
+    public static void main(String[] args) {
+        int num = 20;
+        if (num > 0) {
+            System.out.println("positive");
+        } else if (num < 0) {
+            System.out.println("Negative");
+        } else if (num < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("zero");
+        }
+    }
+
+}
