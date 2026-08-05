8a7ba3f1eb2ad1f0d6fb55532209b3b4050e5577
shrushtitambe048-design <shrushtitambe048@gmail.com>
2026-08-05 12:40:56 +0530

    DAy4 Task

---
+++ track/MO2/program2.java
@@ -0,0 +1,13 @@
+package track.MO2;
+
+public class program2 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 5) {
+            System.out.println(i);
+            i++;
+
+        }
+    }
+
+}
