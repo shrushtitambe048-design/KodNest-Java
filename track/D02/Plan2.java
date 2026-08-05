26b65d80caeff90f6641d3a51b0377b21945d1c7
shrushtitambe048-design <shrushtitambe048@gmail.com>
2026-08-05 10:19:19 +0530

    DAY1 Task

---
+++ track/D02/Plan2.java
@@ -0,0 +1,10 @@
+package track.D02;
+
+public class Plan2 {
+    public static void main(String[] args) {
+        System.out.println("Java: 10");
+        System.out.println("Aptitude 5");
+        System.out.println("Total: 15");
+    }
+
+}
