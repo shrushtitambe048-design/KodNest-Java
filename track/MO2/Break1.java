c32d9ac39c32be872140494466c86adcaecbe3d9
shrushtitambe048-design <shrushtitambe048@gmail.com>
2026-08-06 12:31:03 +0530

    break1

---
+++ track/MO2/Break1.java
@@ -0,0 +1,13 @@
+package track.MO2;
+
+public class Break1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+            if (i == 6) {
+                break;
+
+            }
+        }
+    }
+}
