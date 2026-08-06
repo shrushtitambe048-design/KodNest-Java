5fac7fea9aafca8da44120da9df397f09d13afe8
shrushtitambe048-design <shrushtitambe048@gmail.com>
2026-08-06 21:19:06 +0530

    loop1

--- track/MO2/Forloop.java
+++ track/MO2/Forloop.java
@@ -1,5 +1,10 @@
 package track.MO2;
 
 public class Forloop {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
 
 }
