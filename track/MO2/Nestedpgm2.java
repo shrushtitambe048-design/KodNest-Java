1c686be10d504f2550512a651a38204f2f018ac9
shrushtitambe048-design <shrushtitambe048@gmail.com>
2026-08-06 21:30:25 +0530

    pgm2

--- track/MO2/Nestedpgm2.java
+++ track/MO2/Nestedpgm2.java
@@ -1,5 +1,17 @@
 package track.MO2;
 
-public class Nestedloop prg2 {
-    
+public class Nestedpgm2 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 4; j++) {
+                if (i == 2) {
+                    break;
+                }
+                System.out.println("i : " + i + "j :" + j);
+
+            }
+        }
+
+    }
+
 }
