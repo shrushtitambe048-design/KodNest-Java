9a3bf987475a6896a109cdccfe69356c71668bc2
shrushtitambe048-design <shrushtitambe048@gmail.com>
2026-08-06 12:57:19 +0530

    nested1

--- track/MO2/NestedLoop.java
+++ track/MO2/NestedLoop.java
@@ -2,12 +2,13 @@ package track.MO2;
 
 public class NestedLoop {
     public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j < 5; j++) {
-                System.out.println();
+        for (int i = 1; i <= 3; i++) {
+            if (j == 2) {
+                break;
             }
-            System.out.println();
+            System.out.println("i : " + i + "j : " + j);
 
         }
     }
+
 }
