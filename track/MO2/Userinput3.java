61c42dcb27ec071ae38df1c8aec0a70ca453c7d2
shrushtitambe048-design <shrushtitambe048@gmail.com>
2026-08-07 12:04:07 +0530

    userinput3

---
+++ track/MO2/Userinput3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Userinput3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter char :");
+        char ch = scan.next().charAt(2);
+        System.out.println(ch);
+    }
+
+}
