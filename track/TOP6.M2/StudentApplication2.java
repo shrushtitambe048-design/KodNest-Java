7023881430d574509c827d6a1f541fd3bbae7bcd
shrushtitambe048-design <shrushtitambe048@gmail.com>
2026-08-14 12:51:16 +0530

    studentapp2

--- track/TOP6.M2/StudentApplication2.java
+++ track/TOP6.M2/StudentApplication2.java
@@ -1,40 +1,38 @@
-package track.TOP6.M2;
-
 public class StudentApplication2 {
     String name;
     int age;
     double height;
 
-    Student() {
-        name = "Anu"
+    StudentApplication2() {
+        name = "Anu";
         age = 19;
         height = 5.0;
     }
 
-    Student(String name) {
+    StudentApplication2(String name) {
         this.name = name;
-        student(String name,int age,double height) {
-            this.name = name;
-            this.age = age;
-            this.height = height;
-        }
-        void display() {
-            System.out.println(name);
-            System.out.println(age);
-            System.out.println(height);
-        }
     }
 
-    public class StudentApplication2 {
-
-        public static void main(String[] args) {
-            Student s2 = new Student();
-            s2.display();
-            Student s3 = new Student("Shri");
-            s3.display();
-            Student s1 = new Student("Rani", 18, 5.5);
-            s1.display();
-        }
+    StudentApplication2(String name, int age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
     }
 
+    void display() {
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+    }
+
+    public static void main(String[] args) {
+        StudentApplication2 s2 = new StudentApplication2();
+        s2.display();
+
+        StudentApplication2 s3 = new StudentApplication2("Shri");
+        s3.display();
+
+        StudentApplication2 s1 = new StudentApplication2("Shloka", 18, 5.5);
+        s1.display();
+    }
 }
