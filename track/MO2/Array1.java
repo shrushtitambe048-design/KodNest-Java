4138d8577e3e92b0df876bd690c8e5829f5cc9e7
shrushtitambe048-design <shrushtitambe048@gmail.com>
2026-08-17 11:55:50 +0530

    Array1

---
+++ track/MO2/Array1.java
@@ -0,0 +1,12 @@
+public class Array1 {
+    public static void main(String[] args) {
+        int a[] = { 11, 24, 34, 49, 60 };
+
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a[2]);
+        System.out.println(a[3]);
+        System.out.println(a[4]);
+    }
+
+}
