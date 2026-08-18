41cd6447b56d31fabd2c06f1f4711d97923ca8e7
shrushtitambe048-design <shrushtitambe048@gmail.com>
2026-08-18 12:15:01 +0530

    arraysearch

--- track/MO2/Array/Array4.java
+++ track/MO2/Array/Array4.java
@@ -3,5 +3,24 @@ package Array;
 import java.util.Scanner;
 
 public class Array4 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a[] = new int[5];
+        System.out.println("Enter the elements: ");
+        for (int i = 0; i <= a.length - 1; i++) {
+            a[i] = scan.nextInt();
+        }
+        System.out.println("Array elements are ");
+        for (int i = 0; i <= a.length - 1; i++) {
+            System.out.println(a[i] + " ");
+        }
+        int sum = 0;
+        for (int i = 0; i <= a.length - 1; i++) {
+            sum = sum + a[i];
+            System.out.println();
+            System.out.println("Total :" + sum);
+        }
+
+    }
 
 }
